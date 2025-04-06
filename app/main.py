730462da980a834fd6ff95c762ab7d4457763a5e
davebayke <dave@bayke.com>
2025-04-06 14:01:29 +0800

    feat: handle failures with services

--- app/main.py
+++ app/main.py
@@ -10,14 +10,27 @@ app = FastAPI()
 
 @app.get("/hello")
 async def hello():
-    async with grpc.aio.insecure_channel("grpc-hello:50051") as channel:
-        stub = helloworld_pb2_grpc.GreeterStub(channel)
-        response = await stub.SayHello(helloworld_pb2.HelloRequest(name="Leon"))
-        return {"message": response.message}
+    try:
+        async with grpc.aio.insecure_channel("grpc-hello:50051") as channel:
+            stub = helloworld_pb2_grpc.GreeterStub(channel)
+            response = await stub.SayHello(helloworld_pb2.HelloRequest(name="Leon"))
+            return {"message": response.message}
+    except grpc.aio.AioRpcError as err:
+        if err.code() == grpc.StatusCode.UNAVAILABLE:
+            return {'message': 'Service currently unavailable'}
+
+        return {'message': f'Something wrong with service: {err}'}
+
 
 @app.get("/goodbye")
 async def goodbye():
-    async with grpc.aio.insecure_channel("grpc-goodbye:50052") as channel:
-        stub = goodbye_pb2_grpc.FarewellStub(channel)
-        response = await stub.SayGoodbye(goodbye_pb2.GoodbyeRequest(name="Leon"))
-        return {"message": response.message}
+    try:
+        async with grpc.aio.insecure_channel("grpc-goodbye:50052") as channel:
+            stub = goodbye_pb2_grpc.FarewellStub(channel)
+            response = await stub.SayGoodbye(goodbye_pb2.GoodbyeRequest(name="Leon"))
+            return {"message": response.message}
+    except grpc.aio.AioRpcError as err:
+        if err.code() == grpc.StatusCode.UNAVAILABLE:
+            return {'message': 'Service currently unavailable'}
+
+        return {'message': f'Something wrong with service: {err}'}
